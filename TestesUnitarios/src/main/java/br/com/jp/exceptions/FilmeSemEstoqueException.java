fc3c8e4df333a67fd6c4647c5b573087503e45fc
João Paulo Affini Ximenes <joaopauloa.ximenes@hotmail.com>
2021-09-04 11:08:34 -0300

    Recebendo exception parte 2

---
+++ TestesUnitarios/src/main/java/br/com/jp/exceptions/FilmeSemEstoqueException.java
@@ -0,0 +1,7 @@
+package br.com.jp.exceptions;
+
+public class FilmeSemEstoqueException extends Exception {
+
+	private static final long serialVersionUID = 6854440802026662900L;
+
+}
---
+++ TestesUnitarios/src/main/java/br/com/jp/exceptions/LocadoraException.java
@@ -0,0 +1,17 @@
+package br.com.jp.exceptions;
+
+public class LocadoraException extends Exception {
+
+	private static final long serialVersionUID = 6934485960529399668L;
+
+	public LocadoraException() {
+		super();
+	}
+
+	public LocadoraException(String message) {
+		super(message);
+	}
+	
+	
+
+}
