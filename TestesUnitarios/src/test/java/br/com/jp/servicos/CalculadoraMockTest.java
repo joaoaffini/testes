50da1636476acfd8d2bb08ac83b9c34fb2c3d0fc
João Paulo Affini Ximenes <joaopauloa.ximenes@hotmail.com>
2021-09-15 00:10:30 -0300

    Utilizando spy

--- TestesUnitarios/src/test/java/br/com/jp/servicos/CalculadoraMockTest.java
+++ TestesUnitarios/src/test/java/br/com/jp/servicos/CalculadoraMockTest.java
@@ -1,11 +1,69 @@
 package br.com.jp.servicos;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+	
+	
+	@Mock
+	private Calculadora calcMock;
+	
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void devoMostrarADiferencaEntroMockESpy() {
+		//A diferenca entre mock e spy
+		//qdo executa o metodo com o valor esperado, eles retornam o mesmo resultado
+		//qdo vc passa um valor que nao eh esperado na chamada do metodo, o mock sempre retorna um valor
+		//default que no caso de Integer o valor eh 0
+		//o spy efetua a execucao do metodo e retorna um valor calculado que foi executado pelo metodo
+		//o spy so funciona para classes concretas
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(8);
+		//chamando desta forma o spy sempre executa o metodo e depois grava a expectativa de retorno
+		//para evitar a execucao do metodo, devemos alterar a chamada para
+		Mockito.doReturn(8).when(calcSpy).somar(1, 2);
+		// Mockito.when(calcSpy.somar(1, 2)).thenReturn(8);
+		
+		System.out.println("*****VALORES ESPERADOS******");
+		System.out.println("Mock:" + calcMock.somar(1, 2));
+		System.out.println("Spy:" + calcSpy.somar(1, 2));
+		
+		System.out.println("*****VALORES NAO ESPERADOS******");
+		System.out.println("Mock:" + calcMock.somar(1, 5));
+		System.out.println("Spy:" + calcSpy.somar(1, 5));
+		
+		System.out.println("*****MOCK chamando o metodo real******");
+		Mockito.when(calcMock.somar(1, 2)).thenCallRealMethod();
+		Mockito.when(calcSpy.somar(1, 2)).thenReturn(8);
+		
+		System.out.println("Mock:" + calcMock.somar(1, 2));
+		System.out.println("Spy:" + calcSpy.somar(1, 2));
+		
+		//se eu nao quiser que o spy execute um metodo
+		Mockito.doNothing().when(calcSpy).imprime();
+		calcMock.imprime();
+		calcSpy.imprime();
+		
+		
+	}
+	
+	
+	
+	
+	
 	
 	@Test
 	public void teste() {
